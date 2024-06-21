de464b5db7e561f626cdd5b530544ae40b6cdb21
evansutjahjo <evansutjahjo@gmail.com>
2024-06-21 11:59:33 +0800

    convex database, clerk authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://robust-dogfish-16.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://robust-dogfish-16.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig
